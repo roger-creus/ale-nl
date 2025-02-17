22f54a6cfb24b56d0065362b7506a6179792bedd
rogercc <roger.creus-castanyer@mila.quebec>
2025-02-17 11:04:22 -0500

    wip benchmarking on more envs

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -3,6 +3,9 @@
 # List of environments
 envs=(
   "SpaceInvaders"
+  "MsPacman"
+  "Seaquest"
+  "Freeway"
 )
 
 # List of model names
@@ -10,23 +13,24 @@ models=(
   "meta-llama/Llama-3.2-1B-Instruct"
   "Qwen/Qwen2.5-0.5B-Instruct"
   "meta-llama/Llama-3.2-3B-Instruct"
-  "meta-llama/Meta-Llama-3-8B-Instruct"
-  #"deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
   "microsoft/phi-4"
-  "microsoft/Phi-3.5-mini-instruct"
+  "mistralai/Mixtral-8x7B-Instruct-v0.1"
+  #"microsoft/Phi-3.5-mini-instruct"
+  #"meta-llama/Meta-Llama-3-8B-Instruct"
+  #"deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
   #"Qwen/Qwen2.5-14B-Instruct"
   #"deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
   #"mistralai/Mixtral-8x7B-Instruct-v0.1"
 )
 
 # List of context lengths
-context_lengths=(0 1)
+context_lengths=(1)
 
 # List of prompt chains
 prompt_chains=(
   "prompt_chains/simple"
-  "prompt_chains/think_stepbystep"
-  "prompt_chains/cot"
+  # "prompt_chains/think_stepbystep"
+  # "prompt_chains/cot"
 )
 
 temperatures=(
@@ -42,7 +46,7 @@ for env in "${envs[@]}"; do
       for model in "${models[@]}"; do
         for context_length in "${context_lengths[@]}"; do
           echo "Running evaluation for Env: $env, Model: $model, Context Length: $context_length, Prompt Chain: $prompt_chain"
-          python src/agents/eval_agent.py --env_id "$env" --model_name "$model" --context_length "$context_length" --prompt_chain "$prompt_chain" --temperature "$temperature" --num_episodes=3 
+          python src/agents/eval_agent.py --env_id "$env" --model_name "$model" --context_length "$context_length" --prompt_chain "$prompt_chain" --temperature "$temperature" --num_episodes=5
         done
       done
     done
