fbd99c75d33eb314e11c1b1028668d29e96eec8a
rogercc <roger.creus-castanyer@mila.quebec>
2025-02-26 11:22:40 -0500

    wip benchmarking

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -7,6 +7,9 @@ envs=(
   "Seaquest"
   "Freeway"
   "Asterix"
+  "BattleZone"
+  "Bowling"
+  "BeamRider"
 )
 
 # List of model names
@@ -14,7 +17,7 @@ models=(
   "meta-llama/Llama-3.2-1B-Instruct"
   "Qwen/Qwen2.5-0.5B-Instruct"
   "meta-llama/Llama-3.2-3B-Instruct"
-  "microsoft/phi-4"
+  #"microsoft/phi-4"
   #"Qwen/Qwen2.5-14B-Instruct"
   #"mistralai/Mixtral-8x7B-Instruct-v0.1"
   #"microsoft/Phi-3.5-mini-instruct"
@@ -24,7 +27,7 @@ models=(
 )
 
 # List of context lengths
-context_lengths=(0 1)
+context_lengths=(0 1 5)
 
 # List of prompt chains
 prompt_chains=(
@@ -46,7 +49,7 @@ for env in "${envs[@]}"; do
       for model in "${models[@]}"; do
         for context_length in "${context_lengths[@]}"; do
           echo "Running evaluation for Env: $env, Model: $model, Context Length: $context_length, Prompt Chain: $prompt_chain"
-          python src/agents/eval_agent.py --env_id "$env" --model_name "$model" --context_length "$context_length" --prompt_chain "$prompt_chain" --temperature "$temperature" --num_episodes=5
+          python src/agents/eval_agent.py --env_id "$env" --model_name "$model" --context_length "$context_length" --prompt_chain "$prompt_chain" --temperature "$temperature" --num_episodes=3
         done
       done
     done
